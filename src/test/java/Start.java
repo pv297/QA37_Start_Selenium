59874ab46ef4c22da0d90760f3fb32bc9c21ce61
pv297 <p.v.2977187@gmail.com>
2023-03-02 21:04:07 +0200

    save libraries

---
+++ src/test/java/Start.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+    WebDriver wd;
+    @Test
+    public void start(){
+        wd=new ChromeDriver();
+       // wd.get("https:/www.google.com/");
+        wd.navigate().to("https:/www.google.com/");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+        wd.close(); // only one tab if tab only one &  close browser
+        wd.quit(); // all tabs & close browser
+
+    }
+}
+
